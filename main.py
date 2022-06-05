08c3a74029eefc2e69c8e1148cc808cb81349638
Hasnain Ali <hasnainali.2005@hotmail.com>
2022-06-04 20:18:44 -0400

    Made score

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pygame
 import random
+import math
 
 #Initialize pygame
 pygame.init()
-
 
 # Screen
 screen = pygame.display.set_mode((800, 600))
@@ -12,48 +12,80 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Snake Game")
 
 # Snake
-snake_x = 408
-snake_y = 300
+snake_x = []
+snake_y = []
 snake_x_change = 0
-snake_y_change = 0 
+snake_y_change = 0
+snake_length = 1
+
+
+for i in range(snake_length):
+    snake_x.append(408)
+    snake_y.append(300)
+
+# Score Text
+score = 0
+score_font = pygame.font.Font('KdamThmorPro-Regular.ttf', 32)
 
 def snake(x, y):
     '''(int, int) -> None
     Draws a rectangle (the snake) on the screen'''
     pygame.draw.rect(screen, (128, 128, 128), (x, y, 16, 16))
+    
 
 def snake_movement(direction):
-    '''(str) -> float, float
+    '''(str) -> str
     Moves snake according to the direction'''
     global snake_x_change
     global snake_y_change
     if direction == "North":
-        snake_y_change = -0.3
+        snake_y_change = -3
         snake_x_change = 0
     elif direction == "South":
-        snake_y_change = 0.3
+        snake_y_change = 3
         snake_x_change = 0
     elif direction == "East":
-        snake_x_change = 0.3
+        snake_x_change = 3
         snake_y_change = 0
     elif direction == "West":
-        snake_x_change = -0.3
+        snake_x_change = -3
         snake_y_change = 0
 
-    return snake_x_change, snake_y_change
+    return direction
 
+def apple_position():
+    '''() -> int, int
+    Gives a random coordinate for the apple spawn'''
+    return random.randint(0, 784), random.randint(0, 584)
 
-
-# Apple
-apple_x = random.randint(0, 800)
-apple_y = random.randint(0, 600)
 def apple(x, y):
     '''(int, int) -> None
     Randomizes x and y coordinates and draws apple randomly on the screen'''
     apple = pygame.image.load("apple.png")
     screen.blit(apple, (x, y))
 
+def isCollision(snakeX, snakeY, appleX, appleY):
+    '''(int, int, int, int) -> bool
+    Takes in coordinates of the snake and the apple and uses distance formula to calculate distance between the two then returns True if within 16 pixels'''
+    distance = math.sqrt((math.pow(snakeX - appleX, 2)) + (math.pow(snakeY - appleY, 2)))
+    if distance < 16:
+        return True
+    return False
 
+def score_text(x = 10, y = 10):
+    '''(int, int) -> None
+    Renders the text for the score'''
+    text = score_font.render("Score: " + str(score), True, (255, 255, 255))
+    screen.blit(text, (x, y))
+
+
+
+#Apple Position
+apple_x, apple_y = apple_position()
+
+# FPS
+fps = 60
+clock = pygame.time.Clock()
 
 # Run loop
 running = True
@@ -63,43 +95,60 @@ while running:
     # BG Colour
     screen.fill((127, 255, 0))
 
+    # Score Text
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         # Movement keys
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                snake_x_change, snake_y_change = snake_movement("West")
+                direction = snake_movement("West")
             elif event.key == pygame.K_RIGHT:
-                snake_x_change, snake_y_change = snake_movement("East")
+                direction = snake_movement("East")
             elif event.key == pygame.K_UP:
-                snake_x_change, snake_y_change = snake_movement("North")
+                direction = snake_movement("North")
             elif event.key == pygame.K_DOWN:
-                snake_x_change, snake_y_change = snake_movement("South")
+                direction = snake_movement("South")
             # Quit key (Escape)
             elif event.key == pygame.K_ESCAPE:
                 running = False
-            else:
-                snake_x_change, snake_y_change = snake_movement("North")
+            elif event.key == pygame.K_SPACE:
+                direction = snake_movement("North")
 
     # Boundaries
-    snake_x += snake_x_change
-    if snake_x < 0:
-        running = False
-    elif snake_x > 784:
-        running = False
-    snake_y += snake_y_change
-    if snake_y < 0:
-        running = False
-    elif snake_y > 584:
-        running = False
-    
-    snake_x += snake_x_change
-    snake_y += snake_y_change
+    for i in range(snake_length):
+        snake_x[i] += snake_x_change
+        for j in range(1):
+            snake_x.append(snake_x[i])
+        if snake_x[i] < 0:
+            running = False
+        elif snake_x[i] > 784:
+            running = False
+        snake_y[i] += snake_y_change
+        for j in range(1):
+            snake_y.append(snake_y[i])
+        if snake_y[i] < 0:
+            running = False
+        elif snake_y[i] > 584:
+             running = False
 
-    snake(snake_x, snake_y)
+
+        snake(snake_x[i], snake_y[i])
+
+
+        collision = isCollision(snake_x[i], snake_y[i], apple_x, apple_y)
+        if collision == True:
+            apple_y = 2000
+            apple_x, apple_y = apple_position()
+            score += 1
+            snake_length += 1
+        
+        
     apple(apple_x, apple_y)
+    score_text()
     pygame.display.update()
+    clock.tick(fps)
 
                 
 
